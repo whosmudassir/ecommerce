f83b672db76c6533fce0a9ea602a88358c8ffb4b
whosmudassir <deadsoulartist@gmail.com>
2022-05-20 11:29:34 +0530

    feat: added rainbowui, navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="nav-bar-wrapper">
+          <button class="hamburger-icon">
+            {" "}
+            <i class="fa fa-bars icon" id="render-icon"></i>
+          </button>
+          <div class="left-content">
+            <a href="/index.html">
+              {" "}
+              <p class="heading-medium">Name</p>{" "}
+            </a>
+          </div>
+          <div class="right-content">
+            <button class="icon-wrapper">
+              <i class="fa fa-moon icon"></i>
+            </button>
+            <button class="icon-wrapper">
+              <i class="fa fa-github icon"></i>
+            </button>
+          </div>
+        </div>
       </header>
     </div>
   );
