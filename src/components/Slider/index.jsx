6e1e166764b2d711919f0b92cef5ce8a35e009c4
whosmudassir <deadsoulartist@gmail.com>
2022-05-21 16:01:08 +0530

    fix: rearranging files

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -3,12 +3,18 @@ import React from "react";
 const Slider = () => {
   return (
     <div>
-      <div class="heading-text-wrapper">
-        <p>Filters</p>
-        <p>Clear</p>
-      </div>
-      <div class="categories-wrapper">
-        <p>All Categories</p>
+      <div id="trigger-slider" class="list-box-close list-box">
+        <div class="category-divider">
+          <div class="heading-text-wrapper">
+            <p>Filters</p>
+            <p>Clear</p>
+          </div>
+        </div>
+        <div class="category-divider">
+          <div class="categories-wrapper">
+            <p>All Categories</p>
+          </div>
+        </div>
       </div>
     </div>
   );
