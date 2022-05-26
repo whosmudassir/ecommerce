8abfbceef2d8cca4ce368a5800ecac0caf44b209
whosmudassir <deadsoulartist@gmail.com>
2022-05-26 19:31:28 +0530

    feat: carousel added

--- src/data/carouselData.js
+++ src/data/carouselData.js
@@ -1,18 +1,17 @@
 export const carouselData = [
   {
     id: 1,
-    imgSource:
-      "https://assets.bonkerscorner.com/uploads/2021/07/01173022/IMG-5846-1536x532.jpg",
+    img: "https://assets.bonkerscorner.com/uploads/2021/07/01173022/IMG-5846-1536x532.jpg",
     onClickUrl: "",
   },
   {
     id: 2,
-    img: "https://static.zara.net/photos/2022/V/M/2/p/0000/003/095/2/w/1338/0000003095_15_3_1.jpg?ts=1652430249433",
+    img: "https://assets.bonkerscorner.com/uploads/2021/06/29151211/IMG_4738.jpg",
     onClickUrl: "",
   },
   {
     id: 3,
-    img: "https://static.zara.net/photos/2022/V/M/2/p/0000/003/095/2/w/1338/0000003095_15_3_1.jpg?ts=1652430249433",
+    img: "https://assets.bonkerscorner.com/uploads/2021/06/29151206/IMG_4735.jpg",
     onClickUrl: "",
   },
 ];
