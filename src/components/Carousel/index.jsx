3016df991feec26dc4bd18cee548ad828d272cd5
whosmudassir <deadsoulartist@gmail.com>
2022-05-26 16:42:45 +0530

    fix: seperated carousel data

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { carouselData } from "../../data/carouselData";
 
 const Carousel = () => {
-  return <div>Carousel</div>;
+  const [carouselImgSource, setCarouselImgSource] = useState([]);
+  // const []
+  useEffect(() => {
+    setCarouselImgSource(
+      carouselData.map((carousel) => {
+        return carousel.img;
+      })
+    );
+  }, []);
+  console.log("imgs-->", carouselData);
+  return (
+    <div class="carousel-wrapper">
+      <div class="carousel">{/* <img src={}/> */}</div>
+    </div>
+  );
 };
 
 export default Carousel;
