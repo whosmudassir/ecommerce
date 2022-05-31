034660f237acaaf08856c346713553df0b14050a
whosmudassir <deadsoulartist@gmail.com>
2022-05-31 11:45:45 +0530

    feat: category box

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./index.css";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  return (
+    <div class="category-wrapper">
+      <img
+        class="responsive-img"
+        src="https://i.pinimg.com/564x/67/43/3e/67433e36150113b51814d34a7bc0abfe.jpg"
+      />
+      <div className="category-name">T-shirts</div>
+    </div>
+  );
 };
 
 export default Categories;
