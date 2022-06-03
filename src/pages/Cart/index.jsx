6d3931469ca70e5b75c8754517154329e0b6d9ba
whosmudassir <deadsoulartist@gmail.com>
2022-06-03 08:51:41 +0530

    feat: cart page ui

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./index.css";
 
 const Cart = () => {
-  return <div class="body-wrapper">Cart</div>;
+  return (
+    <div class="body-wrapper">
+      <div className="cart-wrapper">
+        <div>
+          <p className="cart-heading">Cart</p>
+        </div>
+        <div className="order-status-parent-box">
+          <div className="order-status-child-box active">
+            <p className="order-status-child-title">Shopping Cart</p>
+            <p className="order-status-child-body">View your items</p>
+          </div>
+          <div className="order-status-child-box">
+            <p className="order-status-child-title">Shipping and Checkout</p>
+            <p className="order-status-child-body">Enter your details</p>
+          </div>
+          <div className="order-status-child-box">
+            <p className="order-status-child-title">Order Confirmation</p>
+            <p className="order-status-child-body">Review your order</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
