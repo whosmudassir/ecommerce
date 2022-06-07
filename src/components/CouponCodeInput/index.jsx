8de3990439573ff30edbe5f0f100f486f964ffcf
whosmudassir <deadsoulartist@gmail.com>
2022-06-07 11:25:11 +0530

    fix: width on coupon input

--- src/components/CouponCodeInput/index.jsx
+++ src/components/CouponCodeInput/index.jsx
@@ -4,7 +4,11 @@ import "./index.css";
 const CouponCodeInput = () => {
   return (
     <div className="coupon-code-input-wrapper">
-      <input class="outlined-input" type="text" placeholder="Coupon Code" />
+      <input
+        class="outlined-input coupon-input"
+        type="text"
+        placeholder="Coupon Code"
+      />
       <button class="primary-btn coupon-btn"> Apply </button>
     </div>
   );
