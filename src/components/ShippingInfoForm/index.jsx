0bdbf133ad66f4d4ff9be1addcbb050d604f45e3
whosmudassir <deadsoulartist@gmail.com>
2022-06-12 18:49:02 +0530

    fix: border radius on focus

--- src/components/ShippingInfoForm/index.jsx
+++ src/components/ShippingInfoForm/index.jsx
@@ -6,7 +6,7 @@ const ShippingInfoForm = () => {
   return (
     <div className="shipping-info-form-wrapper">
       <p className="order-info-text">Billing & shipping</p>
-      <div>
+      <div className="shipping-info-two-item-wrapper">
         <input
           class="outlined-input"
           type="text"
@@ -20,43 +20,48 @@ const ShippingInfoForm = () => {
           autoComplete="cc-family-name"
         />
       </div>
-      <p>Street address</p>
-      <div>
+      <p className="shipping-info-headings-text">Street address</p>
+      <div className="shipping-info-item-wrapper">
         <input
-          class="outlined-input"
+          class="outlined-input shipping-info-input"
           type="text"
           placeholder="House number and street name"
           autoComplete="street-address"
         />
       </div>
-      <p>CITY/TOWN</p>
-      <div>
+      <p className="shipping-info-headings-text">City / town</p>
+      <div className="shipping-info-item-wrapper">
         <input
-          class="outlined-input"
+          class="outlined-input  shipping-info-input"
           type="text"
           autoComplete="address-level2"
         />
       </div>
-      <p>STATE</p>
-      <div>
-        <input class="outlined-input" type="text" placeholder="" />
-      </div>
-      <p>PIN CODE / ZIP</p>
-      <div>
-        <input class="outlined-input" type="text" autocomplete="postal-code" />
+
+      <p className="shipping-info-headings-text">Pin code / zip</p>
+      <div className="shipping-info-item-wrapper">
+        <input
+          class="outlined-input  shipping-info-input"
+          type="text"
+          autocomplete="postal-code"
+        />
       </div>
 
-      <p>PHONE</p>
-      <div>
+      <p className="shipping-info-headings-text">Phone</p>
+      <div className="shipping-info-item-wrapper">
         <input
-          class="outlined-input"
+          class="outlined-input  shipping-info-input"
           type="text"
           placeholder="Enter 10 digit phone number"
         />
       </div>
-      <p>EMAIL</p>
-      <div>
-        <input class="outlined-input" type="text" autoComplete="email" />
+      <p className="shipping-info-headings-text">Email</p>
+      <div className="shipping-info-item-wrapper">
+        <input
+          class="outlined-input  shipping-info-input"
+          type="text"
+          autoComplete="email"
+        />
       </div>
     </div>
   );
