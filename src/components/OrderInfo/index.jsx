9de006fb90614e5db1b95c34fd0b770210d4860b
whosmudassir <deadsoulartist@gmail.com>
2022-06-12 16:11:55 +0530

    feat: order info in checkout page

---
+++ src/components/OrderInfo/index.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./index.css";
+
+const OrderInfo = () => {
+  return (
+    <div className="order-info-wrapper">
+      <div className="order-info-items-wrapper">
+        <div className="order-info-items">
+          <p class="order-info-text">Order Info</p>
+        </div>
+        <div className="order-info-items">
+          <p class="order-info-item-main-text">Product detail</p>
+          <p class="order-info-item-text">444</p>
+        </div>
+        <div className="order-info-items">
+          <p class="order-info-item-main-text">Discount</p>
+          <p class="order-info-item-text">566</p>
+        </div>
+        <div className="order-info-items">
+          <p class="order-info-item-main-text">Shipping</p>
+          <p class="order-info-item-text">99</p>
+        </div>
+        <div className="order-info-items">
+          <p class="order-info-item-main-text">SGST</p>
+          <p class="order-info-item-text">12</p>
+        </div>
+        <div className="order-info-items">
+          <p class="order-info-item-main-text">CGST</p>
+          <p class="order-info-item-text">12</p>
+        </div>
+        <div className="order-info-items">
+          <p class="order-info-item-main-text">Total</p>
+          <p class="order-info-item-main-text">3312</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderInfo;
