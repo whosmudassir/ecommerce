c4bb62b685222324d58671a03d7f6ac403a323df
whosmudassir <deadsoulartist@gmail.com>
2022-06-14 12:07:42 +0530

    feat: login register forms

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import LoginForm from "../../components/LoginForm";
+import RegisterForm from "../../components/RegisterForm";
 
 const User = () => {
-  return <div>User</div>;
+  return (
+    <div class="body-wrapper">
+      <LoginForm />
+      <RegisterForm />
+    </div>
+  );
 };
 
 export default User;
