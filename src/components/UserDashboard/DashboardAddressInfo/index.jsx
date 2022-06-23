2e24e26c9f03fbe9511ac88517f18b3a9f3078b7
whosmudassir <deadsoulartist@gmail.com>
2022-06-23 12:57:28 +0530

    feat: address ui

--- src/components/UserDashboard/DashboardAddressInfo/index.jsx
+++ src/components/UserDashboard/DashboardAddressInfo/index.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./index.css";
+import DashboardUserAddress from "../DashboardUserAddress";
 
 const DashboardAddressInfo = () => {
   return (
     <div className="dashboard-address-wrapper">
       <p className="dashboard-address-latest-text">Latest Used Addresses</p>
       <p className="dashboard-address-title">Billing address</p>
-      <button>Edit</button>
-      <p className="dashboard-address-text">Mudassir Khan</p>
-      <p className="dashboard-address-text">
-        H.no.-43/116,N.R.peta,kurnool. Opp Sri lakshmi high school Kurnool
-        518001 Andhra Pradesh
-      </p>
+      <div className="dashboard-user-address-wrapper">
+        <DashboardUserAddress />
+        <DashboardUserAddress />
+        <DashboardUserAddress />
+      </div>
+      <button>Add new address</button>
     </div>
   );
 };
