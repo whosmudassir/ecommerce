7aa1e2043b3accf0cbd4ba445ba8e7397e91b6b7
whosmudassir <deadsoulartist@gmail.com>
2022-07-12 09:45:28 +0530

    feat: added select size to store

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../assets/images/logo.jpg";
 import { Link, Outlet } from "react-router-dom";
-import cartStore from "../../store/";
+import { cartStore } from "../../store/";
 
 const NavBar = () => {
   const itemsInCart = cartStore((state) => state.cart);
