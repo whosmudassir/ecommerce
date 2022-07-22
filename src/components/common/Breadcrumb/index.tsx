d9fc9149fcc99edbeecad6f55e4114702f4db595
whosmudassir <deadsoulartist@gmail.com>
2022-07-22 09:58:03 +0530

    feat: breadcrumb

--- src/components/common/Breadcrumb/index.tsx
+++ src/components/common/Breadcrumb/index.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
 
 const Breadcrumb = () => {
-  return <div>Breadcrumb</div>;
+  return (
+    <div className="breadcrumb-wrapper">
+      <Link to={"/"}>
+        <p className="breadcrumb-text">Home</p>
+      </Link>
+      <i class="fa-solid fa-angle-right"></i>
+      <Link to={"/shop"}>
+        <p className="breadcrumb-text">Shop</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Breadcrumb;
