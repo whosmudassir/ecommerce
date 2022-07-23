26c8127f2f6c133cdd651c69ab60eeee73c35ddf
whosmudassir <deadsoulartist@gmail.com>
2022-07-23 20:53:01 +0530

    feat: add to bag btn css

--- src/components/common/AddToBagButton/index.tsx
+++ src/components/common/AddToBagButton/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const AddToBagButton = () => {
-  return <div>AddToBagButton</div>;
+  return (
+    <div>
+      <button
+        className={` ${
+          false ? "primary-btn btn-active" : "secondary-btn btn-inactive"
+        }`}
+        // onClick={() => addItemToCart(item)}
+      >
+        Add to bag
+      </button>
+    </div>
+  );
 };
 
 export default AddToBagButton;
