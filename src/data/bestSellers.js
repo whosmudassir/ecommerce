af7f5d9e28816f61d218a53b51d67b9bad15161c
whosmudassir <deadsoulartist@gmail.com>
2022-09-01 19:59:20 +0530

    fix: best seller ui on mobile

--- src/data/bestSellers.js
+++ src/data/bestSellers.js
@@ -26,4 +26,13 @@ export const bestSellers = [
     imageUrl:
       "https://i.pinimg.com/564x/96/8f/af/968faf7589d67eff9679f7476fd324ab.jpg",
   },
+  {
+    id: 17,
+    brandName: "numbernine",
+    name: "oversized brown hoodie",
+    price: 2999,
+    category: "hoodies",
+    imageUrl:
+      "https://i.pinimg.com/564x/20/52/a1/2052a10de5134e86470913c3b6361639.jpg",
+  },
 ];
