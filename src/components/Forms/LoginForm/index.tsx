bf7b37c172f81637902ccd81e034980d86865de9
whosmudassir <deadsoulartist@gmail.com>
2022-09-12 21:08:35 +0530

    feat: login form int

--- src/components/Forms/LoginForm/index.tsx
+++ src/components/Forms/LoginForm/index.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Modal from "../../common/ Modal";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const content = (
+    <div>
+      <p>form</p>
+    </div>
+  );
+
+  const footer = (
+    <div>
+      <button>ok</button>
+    </div>
+  );
+
+  return (
+    <>
+      <Modal heading={"Log in"} content={content} footer={footer} />
+    </>
+  );
 };
 
 export default LoginForm;
