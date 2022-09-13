5159dd34495bfc7e0100df3c162e8fb9e097d14a
whosmudassir <deadsoulartist@gmail.com>
2022-09-13 20:45:58 +0530

    feat: sign up modal ui

--- src/components/Forms/RegisterForm/index.tsx
+++ src/components/Forms/RegisterForm/index.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Modal from "../../common/ Modal";
 
 const RegisterForm = () => {
-  return <div>RegisterForm</div>;
+  const content = (
+    <div>
+      <p>form</p>
+    </div>
+  );
+
+  const footer = (
+    <div>
+      <button>Sign up</button>
+      <button>Cancel</button>
+    </div>
+  );
+
+  return (
+    <>
+      <Modal heading={"Sign up"} content={content} footer={footer} />
+    </>
+  );
 };
 
 export default RegisterForm;
