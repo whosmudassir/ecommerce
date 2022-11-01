2d5f9e9ba7e2a18fed47a32559c51dc0f7921261
whosmudassir <deadsoulartist@gmail.com>
2022-11-01 12:15:24 +0530

    fix: build failures

--- src/components/Forms/AuthForm/index.tsx
+++ src/components/Forms/AuthForm/index.tsx
@@ -1,45 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import Modal from "../../common/ Modal";
 import { userLogin } from "../../../store";
 import "./index.css";
-import { auth } from "../../../firebase-config";
-import { signInWithEmailAndPassword } from "firebase/auth";
 import Login from "../../../pages/Login";
 
 const AuthForm = () => {
-  const initialState = {
-    email: "",
-    password: "",
-  };
-
   //store
   const hideAuthModal = userLogin((state: any) => state.hideAuthModal);
 
-  const [formValue, setFormValue] = useState(initialState);
-  const [errorMessage, setErrorMessage] = useState("");
-
   const triggerModalClose = () => {
     hideAuthModal();
-  };
-
-  const handleChange = (e: any) => {
-    const { name, value } = e.target;
-    setFormValue({ ...formValue, [name]: value });
-  };
-
-  const loginUser = async () => {
-    try {
-      await signInWithEmailAndPassword(
-        auth,
-        formValue.email,
-        formValue.password
-      );
-      triggerModalClose();
-    } catch (e) {
-      console.log(e);
-      //@ts-ignore
-      setErrorMessage(e.code);
-    }
   };
 
   const content = (
