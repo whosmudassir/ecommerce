29bdef6918e1fd63473625f6e93f4c509e32605c
whosmudassir <deadsoulartist@gmail.com>
2022-11-06 12:28:43 +0530

    feat: username fetch in dashboard

--- src/components/UserDashboard/DashboardBody/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./index.css";
-
-const DashboardBody = () => {
-  return <div className="dashboard-content-wrapper">Work in progress😁</div>;
-};
-
-export default DashboardBody;
