26040f4eb73d8705fa80c4997463c40dbab67cde
whosmudassir <deadsoulartist@gmail.com>
2022-11-17 08:59:03 +0530

    fix: added total text on price

--- src/components/UserDashboard/DashboardOrderInfo/index.tsx
+++ src/components/UserDashboard/DashboardOrderInfo/index.tsx
@@ -65,7 +65,7 @@ const DashboardOrderInfo = () => {
                   <p className="cart-product-item-title">Price</p>
                   <p className="cart-product-item-child-item">
                     {/* @ts-ignore */}
-                    {order[0].price}
+                    Total: {order[0].price}
                   </p>
                 </div>
               </div>
